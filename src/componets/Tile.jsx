e9151bec590d6085faedb01c548141540136b0fa
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-11-05 11:30:06 -0800

    fixed bug with rotating tiles set up to start adding pieces

--- src/componets/Tile.jsx
+++ src/componets/Tile.jsx
@@ -4,8 +4,16 @@ import React from 'react';
 const Tile = (props)=>{
   const rotation = props.tileRotation*90;
   return(
-    <div className='tile'>
-      <img src={props.tilePic} style={{transform: `rotate(${rotation}deg)`}}/>
+    <div className='tile' style={{backgroundImage: `url(${props.tilePic})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    transform: `rotate(${rotation}deg)`
+    }}>
+      <div className='pieceContainer' style={{transform: `rotate(${-rotation}deg)`}}>
+        <p >
+          test is this up right
+        </p>
+      </div>
     </div>
   )
 }
