af1ea28ef1b92730ff77d6f1503fcf8e65787f37
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-11-26 18:47:08 -0800

    added function to enable rotaion for reveal

--- src/componets/Tile.jsx
+++ src/componets/Tile.jsx
@@ -2,8 +2,11 @@ import React, { useContext } from 'react';
 import general from '../playerFunctions/general';
 import { GameContext } from '../context/GameContext';
 
-function test(Id,loc){
-  const spots = [loc]
+function test(Id,loc,rot){
+  // id is game num
+  // loc is the tile num 
+  // rot is the rotation of the tile
+  const spots = [[loc,rot]]
   general.revealTile(Id[0],spots);
 }
 
@@ -17,7 +20,7 @@ const Tile = (props)=>{
     transform: `rotate(${rotation}deg)`
     }}
     //props num is location of the tile
-    onClick={()=>test(gameID,props.num)}>
+    onClick={()=>test(gameID,props.num,3)}>
       <div className='pieceContainer' style={{transform: `rotate(${-rotation}deg)`}}>
         <div className='characters'>
           {props.characterIcons.map((e, k)=>{
